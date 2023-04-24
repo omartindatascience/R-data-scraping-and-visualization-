908a0883d1ebfab06548acaa120b6af1b835da51
omartindatascience <131703698+omartindatascience@users.noreply.github.com>
2023-04-24 20:37:10 +0200

    Update Sample of use.R

--- Sample of use.R
+++ Sample of use.R
@@ -1,5 +1,5 @@
 
-#Cargamos las librerías 
+#We load the libraries
 
 library(rvest)
 library(ggplot2)
@@ -18,11 +18,11 @@ library(stringr)
 library(knitr) 
 library(DT) 
 
-# Establecemos la configuración regional de idioma
+# We set the regional language configuration to English
 
 Sys.setlocale("LC_TIME", "English")
 
-# 1. Función para obtener un data frame de reviews de amazon por código de producto.
+# Function to obtain a data frame of Amazon reviews by product code
   
  getReviewsFromAmazon <- function(product_codes, product_names = c()){
   final_table <- matrix(ncol = 4, nrow = 1) #We define two additional columns, one for date and the other for stars
@@ -60,54 +60,52 @@ Sys.setlocale("LC_TIME", "English")
 }
 
  
-# Obtener data frame usando la función getReviewsFromAmazon
+# Obtain data frame using the getReviewsFromAmazon function
  
 final_table <- getReviewsFromAmazon(product_codes = c("B08WF4XDMF", "B08WJMSS8H"), product_names = c("SamsumgQ80", "SonyX85J"))
 
 
-# 2. Obtención del análisis de sentimiento de las reviews obtenidas.
-
+# Get sentiment analysis of the obtained reviews.
 sentiment_review <- sentiment_by(final_table$ReviewText, polarity_dt = hash_sentiment_jockers_rinker)
 head(sentiment_review, 10)
 
-# Concatenamos el data frame original obtenido con la función getReviewsFromAmazon y el de análisis de sentimiento
+# Concatenate the original data frame obtained with the getReviewsFromAmazon function and the sentiment analysis data frame.
 
 final_table <- cbind(final_table, sentiment_review[,-1])
 
-# 3. Plot Avg.Sentiment de las reviews factorizados por Rating
+# Plot Avg.Sentiment
 
 ggplot(final_table, aes(x = Rating, y = ave_sentiment, group = factor(Rating), fill = factor(Rating))) + #specify axis and color vars
-  geom_boxplot() +  # capa de diagrama de caja
-  facet_wrap(~ProductCode) + #separa los gráficos segun su codigo de producto
-  geom_hline(yintercept=0, color = "grey") + #dibujamos una linea en el punto x=0, mostrando el sentimiento neutral
-  ylab("Avg. Sentiment") +  # asignamos el nombre de la axis y
-  xlab("Review Star Rating") + # asignamos el nombre a la axis x
-  ggtitle("Sentiment of the selected Products by Star Rating") # asignamos un titulo al gráfico
+  geom_boxplot() +  # boxplot layer
+  facet_wrap(~ProductCode) + # separates the graphics according to product code
+  geom_hline(yintercept=0, color = "grey") + # we draw a line at the point x=0, showing the neutral sentiment
+  ylab("Avg. Sentiment") +  # we assign the axis name of y
+  xlab("Review Star Rating") + # we assign the name of the axis y
+  ggtitle("Sentiment of the selected Products by Star Rating") # assign a title to the graphic
 
-# Calculamos la el coeficiente de correlación de Pearson entre la puntución del análisis de setimiento y el rating.
+# We calculated the Pearson correlation coefficient between the sentiment analysis score and the rating.
 
 correlation <- cor(final_table$ave_sentiment, final_table$Rating) 
 print(correlation)
 
 
-# 4. Visualización de las reviews de los productos a lo largo del tiempo
+# Visualization of product reviews over time
 
 final_table$Date <- as.Date(as.character(final_table$Date), "%B %d, %Y")
 date_sentiment <- aggregate(ave_sentiment ~ Date + ProductCode, final_table, mean) #compute the average sentiment for each date and product
 
 ggplot(date_sentiment, aes(x = Date, y = ave_sentiment)) +
   geom_smooth(method="loess", size=1, se=T, span = .5) +
-  facet_wrap(~ProductCode) + #producir gráficos distintos segun codigos de producto
-  geom_hline(yintercept=0, color = "grey") + # línea gris en x = 0, para marcar sentimiento neutral
-  ylab("Avg. Sentiment") +  # nombre del eje y
-  xlab("Date") + #nombre del eje x
+  facet_wrap(~ProductCode) + # we produce different graphics according to product codes
+  geom_hline(yintercept=0, color = "grey") + # grey line at x = 0, to mark neutral sentiment
+  ylab("Avg. Sentiment") +  # axis name y
+  xlab("Date") + # axis name x
   ggtitle("Sentiment of Amazon Reviews over time by Product")
 
 
-# 5. Generación de un wordcloud con las principales palabras por productos
+# Generation of a wordcloud chart with the main words by product.
 
-# Creamos una función llamada getWordcloud, para plotear un wordcloud de un producto que pasamos como parámetro
-
+# We create a function called getWordcloud, to plot a wordcloud of a product that we pass as a parameter
 
 getWordcloud <- function(product_name){
 
@@ -128,28 +126,28 @@ tokens <- tidy_dat %>%
 
 tokens %>% head(10)
 
-data("stop_words") # limpiamos stop words
+data("stop_words") # we clean up stop words
 tokens_clean <- tokens %>%
   anti_join(stop_words)
 
-nums <- tokens_clean %>% filter(str_detect(word, "^[0-9]")) %>% select(word) %>% unique() # eliminamos valores numéricos
+nums <- tokens_clean %>% filter(str_detect(word, "^[0-9]")) %>% select(word) %>% unique() # we eliminate numeric values
 
 tokens_clean <- tokens_clean %>% 
   anti_join(nums, by = "word")
       
- pal <- brewer.pal(8,"Dark2") # Establecemos una paleta de colores y generamos un wordcloud con el data frame ya procesado.
+ pal <- brewer.pal(8,"Dark2") # We set a color palette and generate a wordcloud with the data frame already processed.
  tokens_clean %>% 
    with(wordcloud(word, n, random.order = FALSE, max.words = 50, colors=pal))  
  
 }
  
-# Generamos gráficos wordcloud llamando a nuestra función getWordcloud para los dos productos que tenemos
+# We generate wordcloud graphics by calling our getWordcloud function for the two products we have
 
 getWordcloud("SonyX85J")
 getWordcloud("SamsumgQ80")
 
 
-# 6. Interpretación de resultados: Se adjunta en archivo pdf,junto con los gráficos generados. 
+
  
  
  
